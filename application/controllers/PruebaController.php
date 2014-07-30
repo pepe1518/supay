8195138ac2c738ab0f0a9488f039f64ea1fba793
cahirulo <pepe1518@gmail.com>
2014-07-29 22:35:24 -0400

    [controlador de prueba] creando un controlador para hacer pruebas sobre distintas funcionalidades

---
+++ application/controllers/PruebaController.php
@@ -0,0 +1,24 @@
+<?php
+
+class PruebaController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        echo "hola vamos a probar funciones de shell";
+		
+		exec('"C:\Program Files\Git\bin\sh.exe" --login -i');
+		
+		$salida = exec('mkdir ..\gits\liz.git');
+
+		$this->view->salida = $salida;
+    }
+
+
+}
+
